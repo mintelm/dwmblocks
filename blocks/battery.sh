ce534d12b82fa2aca7b536bfaaa000bc58696bb1
Mario Mintel <mariomintel@gmail.com>
2020-08-09 20:10:35 +0200

    rice battery block

--- blocks/battery.sh
+++ blocks/battery.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 read -r capacity </sys/class/power_supply/BAT0/capacity
-echo " ${capacity}%"
+read -r batstatus < /sys/class/power_supply/BAT0/status
+
+if [ "$batstatus" = "Charging" ]; then
+        charging="ﮣ "
+fi
+
+if [ "$capacity" -gt 90 ]; then
+        echo "${charging} ${capacity}%"
+elif [ "$capacity" -gt 65 ]; then
+        echo "${charging} ${capacity}%"
+elif [ "$capacity" -gt 40 ]; then
+        echo "${charging} ${capacity}%"
+elif [ "$capacity" -gt 10 ]; then
+        echo "${charging} ${capacity}%"
+else
+        echo "${charging} ${capacity}%"
+fi
