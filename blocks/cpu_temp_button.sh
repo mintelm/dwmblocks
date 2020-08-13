a00976c0696eca374a8cf9574db324459b620149
Mario Mintel <mariomintel@gmail.com>
2020-08-14 00:44:15 +0200

    use st instead of alacritty

--- blocks/cpu_temp_button.sh
+++ blocks/cpu_temp_button.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case "$1" in
-    1) exec alacritty -e "ytop" ;;
-    2) exec alacritty -e "ytop" ;;
-    3) exec alacritty -e "ytop" ;;
+    1) exec st -e "ytop" ;;
+    2) exec st -e "ytop" ;;
+    3) exec st -e "ytop" ;;
 esac
--- blocks/time_calendar_button.sh
+++ blocks/time_calendar_button.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case "$1" in
-    1) exec alacritty -e "calcurse" ;;
-    2) exec alacritty -e "calcurse" ;;
-    3) exec alacritty -e "calcurse" ;;
+    1) exec st -e "calcurse" ;;
+    2) exec st -e "calcurse" ;;
+    3) exec st -e "calcurse" ;;
 esac
