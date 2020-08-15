9f3c5ed27337ab51ed120c40728670e6c05b8e72
Mario Mintel <mariomintel@gmail.com>
2020-08-15 19:44:37 +0200

    dont sigdwmblocks on upgrades, as it can be done via pacman hook

--- blocks/arch_pkg_button.sh
+++ blocks/arch_pkg_button.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case "$1" in
-    1) st -e sh -c 'yay -Syyu; sigdwmblocks 2; printf "\nPress any button to exit."; read';;
+    1) st -e sh -c 'yay -Syyu; printf "\nPress any button to exit."; read';;
     2) yay -Syy; sigdwmblocks 2;;
-    3) st -e sh -c 'yay -Qu; sigdwmblocks 2; printf "\nPress any button to exit."; read';;
+    3) st -e sh -c 'yay -Qu; printf "\nPress any button to exit."; read';;
 esac
