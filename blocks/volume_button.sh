4530b098744f1876e2dd2c3b3f866008d96bc032
Mario Mintel <mariomintel@gmail.com>
2020-10-22 14:57:53 +0200

    change volume buttons

--- blocks/volume_button.sh
+++ blocks/volume_button.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case "$1" in
-    1) pamixer --toggle-mute ;;
-    2) exec pavucontrol ;;
+    1) exec pavucontrol ;;
+    2) pamixer --toggle-mute ;;
     3) pamixer --set-volume 60 ;;
 esac
